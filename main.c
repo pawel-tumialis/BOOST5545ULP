22ebbfe7c4dc610a7cdbac2ff6f0ca70a17258db
Paweł Tumialis <67554101+pawel-tumialis@users.noreply.github.com>
2023-01-02 18:54:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include "button.h"
 #include "oled.h"
 
-#define SAMPLE_NUMBER 1024
+// ------------------STUDENT------------------------------
+#define SAMPLE_NUMBER 1024      //jeśli użyta ma być funkcja hwafft_128pts to tutaj trzeba wpisać 128 itd.
+
 CSL_DMA_Handle      dmaHandleRxL;
 CSL_DMA_Handle      dmaHandleRxR;
 CSL_DMA_Handle      dmaHandleTxL;
@@ -28,13 +30,13 @@ typedef long        Int32_;
 #define REAL 0
 #define IMAG 1
 
-Uint32 tab_fft[1024];
+Uint32 tab_fft[SAMPLE_NUMBER];
 Int32 kk = 0, jj = 0;
 
 #pragma DATA_SECTION(tab_br_buf, ".data_br_buf");  // tablica przypisana do bloku pamieci ktorego aders poczatkowy ma przynajmniej 12 zer najmniej znaczacych.
-Uint32 tab_br_buf[1024];                           // Pamiec ta wydzielono w pliku linkera lnkx.cmd
+Uint32 tab_br_buf[SAMPLE_NUMBER];                           // Pamiec ta wydzielono w pliku linkera lnkx.cmd
 
-Uint32 tab_scratch_buf[1024];
+Uint32 tab_scratch_buf[SAMPLE_NUMBER];
 
 typedef union { Int32_ x_i32;  Uint32 x_ui32; Int16 tab_i16[2]; } data_type;
 data_type dat;
@@ -47,13 +49,18 @@ Int32_ *scratch = (Int32_ *)tab_scratch_buf;
 Uint16 out_sel;
 Int32_ *result;
 Int16 *result_16;
-Int32 pow[512];
-Int16 pow_fl[512];
+Int32 pow[SAMPLE_NUMBER/2];
+Int16 pow_fl[SAMPLE_NUMBER/2];
 
 Int16 real, imag;
 
-void hwafft_br(Int32_ *data, Int32_ *data_br, Uint16 data_len);                            // funkcje przypisane do adresow w ROM
-Uint16 hwafft_1024pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);  // na koncu pliku linkera lnkx.cmd
+Uint16 hwafft_1024pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);
+Uint16 hwafft_512pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);
+Uint16 hwafft_128pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);
+Uint16 hwafft_64pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);
+Uint16 hwafft_32pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);
+Uint16 hwafft_16pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);
+Uint16 hwafft_8pts(Int32_ *data, Int32_ *scratch, Uint16 fft_flag, Uint16 scale_flag);
 
 extern void VECSTART(void);
 interrupt void dma_isr(void);
@@ -116,7 +123,7 @@ TEST_STATUS initialize_dma(void){
         dmaConfig.dmaInt       = CSL_DMA_INTERRUPT_ENABLE;
         dmaConfig.chanDir      = CSL_DMA_READ;
         dmaConfig.trfType      = CSL_DMA_TRANSFER_IO_MEMORY;
-        dmaConfig.dataLen      = 1024;
+        dmaConfig.dataLen      = SAMPLE_NUMBER;
         dmaConfig.srcAddr      = (Uint32)&CSL_I2S2_REGS->I2SRXLT0;
         dmaConfig.destAddr       = (Uint32)(samples_left);
 
@@ -132,7 +139,7 @@ TEST_STATUS initialize_dma(void){
         dmaConfig.dmaInt       = CSL_DMA_INTERRUPT_ENABLE;
         dmaConfig.chanDir      = CSL_DMA_WRITE;
         dmaConfig.trfType      = CSL_DMA_TRANSFER_IO_MEMORY;
-        dmaConfig.dataLen      = 1024;
+        dmaConfig.dataLen      = SAMPLE_NUMBER;
         dmaConfig.srcAddr      = (Uint32)samples_left;
         dmaConfig.destAddr       = (Uint32)&CSL_I2S2_REGS->I2STXLT0;
 
@@ -148,7 +155,7 @@ TEST_STATUS initialize_dma(void){
         dmaConfig.dmaInt       = CSL_DMA_INTERRUPT_ENABLE;
         dmaConfig.chanDir      = CSL_DMA_WRITE;
         dmaConfig.trfType      = CSL_DMA_TRANSFER_IO_MEMORY;
-        dmaConfig.dataLen      = 1024;
+        dmaConfig.dataLen      = SAMPLE_NUMBER;
         dmaConfig.srcAddr      = (Uint32)samples_left;
         dmaConfig.destAddr       = (Uint32)&CSL_I2S2_REGS->I2STXRT0;
 
@@ -173,7 +180,7 @@ interrupt void dma_isr(void)
     {
         if(startFFT == 1){
             // HARDWARE FFT
-            for (jj=0; jj<1024; jj++)   {   //  kopiowanie danych wejsciowych
+            for (jj=0; jj<SAMPLE_NUMBER; jj++)   {   //  kopiowanie danych wejsciowych
                         tab_fft[jj] = 0;  // wyzerowanie czesci realis i imaginalis
                         tab_fft[jj] = (Uint32)samples_left[jj] << 16; // wczytanie probki do czesci realis
                     }
@@ -182,25 +189,28 @@ interrupt void dma_isr(void)
                     //scale_flag = SCALE_FLAG;    // zawsze bezpieczne, ale dla sygnalow o malym poziomie amplitudy prazkow moga byc male i ginac w szumie szerokopasmowym
 
                     /* Bit-Reverse 1024-point data, Store into data_br, data_br aligned to 12-least significant binary zeros*/
-                    hwafft_br(data, data_br, 1024); /* bit-reverse input data,
+                    hwafft_br(data, data_br, SAMPLE_NUMBER); /* bit-reverse input data,
 
                     Destination buffer aligned */
                     data = data_br;
 
+                    //------------STUDENT--------------
                     /* Compute 1024-point FFT, scaling enabled. */
                     out_sel = hwafft_1024pts(data, scratch, fft_flag, scale_flag);
+                    // albo inna funkcja np.
+                    //out_sel = hwafft_128pts(data, scratch, fft_flag, scale_flag);
 
                     if (out_sel == OUT_SEL_DATA) {  result = data;  }  // przypisanie do wskaznika result wskaznika bufora wyjsciowego zawierajacego wyniki obliczen
                     else {  result = scratch;  }
 
-                    for (jj=0; jj<512; jj++)   {  // obliczenie kwadratu modulu sygnalu zespolonego widma
+                    for (jj=0; jj<SAMPLE_NUMBER/2; jj++)   {  // obliczenie kwadratu modulu sygnalu zespolonego widma
                         real = (Int16)(result[jj]>>16);  imag = (Int16)(result[jj] & 0x0000FFFF);
                         pow_fl[jj] = (real)*(real) + (imag)*(imag);
                     }
 
             // FFT READY
             status = clear();
-            writeFFT(pow_fl, 512);
+            writeFFT(pow_fl, SAMPLE_NUMBER/2);
             startFFT = 0;
         }
     }
